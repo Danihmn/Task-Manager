e94e986e8fedf519029f73829dc9217d8d7dde0e
Danihmn <daniel.bezerra.mult@gmail.com>
2026-06-21 22:46:37 -0300

    Adiciona teste faltante do endpoints de Hello (que retorna HTML)

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,15 @@ from app import app
 
 def test_read_root():
     with TestClient(app) as client:  # arrange
-        response = client.get('/')  # act
+        response = client.get('tasks/')  # act
 
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Hello, World!'}  # assert
+
+
+def test_read_hello():
+    with TestClient(app) as client:
+        response = client.get('tasks/hello')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h1>Hello, World!</h1>'
